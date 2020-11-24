a381d772672838dd11ae5065d8bd3c8ecd73d2f6
Jikun Liao <liaojikundever@gmail.com>
2020-11-24 11:44:28 -0500

    init

---
+++ app/src/components/common/icons/IconArrowUpInCircle.tsx
@@ -0,0 +1,24 @@
+import React from 'react'
+
+export const IconArrowUpInCircle = () => {
+  return (
+    <svg fill="none" height="40" viewBox="0 0 41 40" width="41" xmlns="http://www.w3.org/2000/svg">
+      <path
+        d="M39.75 20C39.75 30.6227 31.0284 39.25 20.25 39.25C9.47164 39.25 0.75 30.6227 0.75 20C0.75 9.37727 9.47164 0.75 20.25 0.75C31.0284 0.75 39.75 9.37727 39.75 20Z"
+        stroke="#4B9E98"
+        strokeWidth="1.5"
+      />
+      <g clipPath="url(#arrowUpInCircleClip0)">
+        <path
+          d="M12.1504 20L13.578 21.41L19.2379 15.83L19.2379 28L21.2629 28L21.2629 15.83L26.9228 21.41L28.3504 20L20.2504 12L12.1504 20Z"
+          fill="#4B9E98"
+        />
+      </g>
+      <defs>
+        <clipPath id="arrowUpInCircleClip0">
+          <rect fill="white" height="24.3" transform="translate(8.09961 32) rotate(-90)" width="24" />
+        </clipPath>
+      </defs>
+    </svg>
+  )
+}
---
+++ app/src/components/common/icons/IconConvert.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export const IconConvert = () => {
+  return (
+    <svg fill="none" height="22" viewBox="0 0 22 22" width="22" xmlns="http://www.w3.org/2000/svg">
+      <path
+        d="M11 3.66667L9.70746 4.95917L14.8225 10.0833H3.66663V11.9167H14.8225L9.70746 17.0408L11 18.3333L18.3333 11L11 3.66667Z"
+        fill="#86909E"
+      />
+    </svg>
+  )
+}
